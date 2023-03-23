7fa6e457e34a56b82d30ae74416700bb8055e1c0
Arnaud Lefebvre <arnaud.lefebvre@clever-cloud.com>
2023-03-23 18:32:46 +0100

    Client: try to avoid too much allocation

--- src/client.rs
+++ src/client.rs
@@ -20,29 +20,30 @@ impl Client {
         std::thread::spawn(move || {
             let mut sequence: u64 = 0;
             let socket = UdpSocket::bind("0.0.0.0:0").expect("Client should bind");
+            let mut start = std::time::Instant::now();
+            let mut buf = BytesMut::with_capacity(config.packet_size);
 
             loop {
                 if sequence > 0 && sequence % config.packet_rate as u64 == 0 {
                     info!(
-                        "Sent {} paquets, waiting 1 second before next batch",
-                        sequence
+                        "Sent {} paquets in {:?}, waiting 1 second before next batch",
+                        sequence, start.elapsed()
                     );
                     sequence = 0;
                     std::thread::sleep(std::time::Duration::from_secs(1));
+                    start = std::time::Instant::now();
                 }
-                let mut buf = BytesMut::new();
                 buf.put_u64(sequence);
                 buf.put_u64(Utc::now().timestamp_nanos() as u64);
-                buf.resize(config.packet_size, 0);
-                let to_send = buf.freeze();
 
-                socket.send_to(&to_send, config.remote).unwrap();
-                debug!(
-                    "Sent packet size {} to {:?}: {:?}",
-                    to_send.len(),
-                    config.remote,
-                    to_send
-                );
+                socket.send_to(&buf[..], config.remote).unwrap();
+                //debug!(
+                    //"Sent packet size {} to {:?}: {:?}",
+                    //buf.len(),
+                    //config.remote,
+                    //buf
+                //);
+                buf.clear();
 
                 sequence += 1;
             }
